3d41b944d3608369c6d9f9e6ad6a21bbf9eee2de
gladysndich <itsgladysbrian@gmail.com>
2022-09-08 10:15:44 +0300

    New application controllers added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,67 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    
+  end
+
+  get "/teams" do
+    teams = Team.all
+    teams.to_json
+  end
+
+  get "/teams/:id" do
+    team_id = Team.find(params[:id])
+    team_id.to_json
+  end
+
+  get "/players" do
+    players = Player.all
+    players.to_json
+  end
+
+  get "/players/:id" do
+    player_id = Player.find(params[:id])
+    player_id.to_json
+  end
+
+  get "/coaches" do
+    coaches = Coach.all
+    coaches.to_json
+  end
+
+  get "/coaches/:id" do
+    coach_id = Coach.find(params[:id])
+    coach_id.to_json
+  end
+
+  post '/teams' do
+    new_team = Team.create(name: params[:name])
+  end
+
+  post '/coaches' do
+    new_coach = Coach.create(name:params[:name],
+    team_id:params[:team_id])
+    new_coach.to_json
+  end
+
+  post '/players' do
+    new_player = Player.create(name: params[:name],
+    team_id:params[:team_id])
+    new_player.to_json
+  end
+
+  delete '/teams/:id' do
+    ditch_team = Team.find(params[:id])
+    ditch_team.destroy
+    ditch_team.to_json
+  end
+
+  patch '/teams/:id' do
+    updated_team = Team.find(params[:id])
+    updated_team.update(
+      name: params[:name]
+    )
+    updated_team.to_json
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,30 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+#Teams
+team1 = Team.create(name: "Avengers")
+team2 = Team.create(name: "Panthers")
+team3 = Team.create(name: "Wildcats")
+team4 = Team.create(name: "Bears")
+team5 = Team.create(name: "Avengers")
+
+#Coaches
+coach1 = Coach.create(name: "Martin Gitonga", team_id:team1.id)
+coach2 = Coach.create(name: "Charles Mutua", team_id:team2.id)
+coach3 = Coach.create(name: "Frank Omondi", team_id:team3.id)
+coach4 = Coach.create(name: "Wambua Kitema", team_id:team4.id)
+coach5 = Coach.create(name: "Nicholas Bor", team_id:team5.id)
+
+#Players
+player1 = Player.create(name: "Peter Wambugu", team_id:team1.id)
+player2 = Player.create(name: "Jacob Makori", team_id:team1.id)
+player3 = Player.create(name: "Joseph Ronoh", team_id:team2.id)
+player4 = Player.create(name: "Sebastian Ikinya", team_id:team2.id)
+player5 = Player.create(name: "Josphat Wairia", team_id:team3.id)
+player6 = Player.create(name: "Zach Njau", team_id:team3.id)
+player7 = Player.create(name: "Sam Mbugua", team_id:team4.id)
+player8 = Player.create(name: "Dave Njuguna", team_id:team4.id)
+player9 = Player.create(name: "Josiah Nyakundi", team_id:team5.id)
+player10 = Player.create(name: "Bernard Makau", team_id:team5.id)
+
 puts "✅ Done seeding!"
